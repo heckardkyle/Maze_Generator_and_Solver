d72edecb3b97de547091b954f5d935915bfbd478
heckardkyle <heckardkyle@yahoo.com>
2025-03-25 07:37:18 -0700

    Added Window Class. Running Main opens Tk Window.

---
+++ Window.py
@@ -0,0 +1,22 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("title")
+        self.canvas = Canvas()
+        self.canvas.pack()
+        self.window_running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+    
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.window_running = True
+        while self.window_running is True:
+            self.redraw()
+
+    def close(self):
+        self.window_running = False
